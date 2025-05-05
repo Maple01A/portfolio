68d82d48f435b03e5c9dc269822013e01944e63b
Lau Dendoung <k611076b@mails.cc.ehime-u.ac.jp>
2025-05-05 13:54:07 +0900

    Update serverless

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // 画像の最適化をオフにするか、静的対応に切り替え
   images: {
     unoptimized: true,
   },
-  // ベースパスが必要な場合（GitHubPages等）
-  // basePath: '/portfolio',
+  basePath: '/portfolio', // GitHubリポジトリ名を指定
+  assetPrefix: '/portfolio', // GitHubリポジトリ名を指定
 };
 
 export default nextConfig;
